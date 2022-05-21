b1d47d0b4e54647f90672c98122145746f4f3936
achourali <achourali1999@gmail.com>
2022-05-21 20:28:46 +0100

    +task crud

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Task } from './entities/task';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,32 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('tasks')
+  async getAll(): Promise<Task[]> {
+    return this.appService.getAll();
+  }
+
+  @Get('task/:id')
+  async getById(@Param('id') id: number): Promise<Task> {
+    return this.appService.getById(id);
+  }
+
+  @Post('task')
+  async create(@Body() task: Task): Promise<Task> {
+    return this.appService.create(task);
+  }
+
+  @Put('task/:id')
+  async update(@Param('id') id: number, @Body() task: Task): Promise<Task> {
+    task.id = id;
+
+    return this.appService.update(task);
+  }
+
+  @Delete('task/:id')
+  async delete(@Param('id') id: number) {
+    this.appService.delete(id);
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './entities/task';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  tasks: Task[] = [];
+
+  getAll() {
+    return this.tasks;
+  }
+
+  getById(id: number) {
+    const task = this.tasks.find((value) => value.id == id);
+
+    return task;
+  }
+
+  create(task: Task) {
+    let lastId = 0;
+
+    if (this.tasks.length > 0) {
+      lastId = this.tasks[this.tasks.length - 1].id;
+    }
+
+    task.id = lastId + 1;
+    task.completed=false;
+
+    this.tasks.push(task);
+
+    return task;
+  }
+
+  update(task: Task) {
+    const taskArray = this.getById(task.id);
+
+    if (taskArray) {
+      taskArray.description = task.description;
+      taskArray.completed = task.completed;
+    }
+
+    return taskArray;
+  }
+
+  delete(id: number) {
+    const index = this.tasks.findIndex((value) => value.id == id);
+
+    this.tasks.splice(index, 1);
   }
 }
