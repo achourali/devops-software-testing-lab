fa55da3fc5d5800e5aa6406db3f4ec37cc87e92e
achourali <achourali1999@gmail.com>
2022-05-30 20:31:25 +0100

    @frontend: e2e tes: adding new tasks

--- frontend/tests/e2e/specs/test.js
+++ frontend/tests/e2e/specs/test.js
@@ -1,8 +1,35 @@
 // https://docs.cypress.io/api/table-of-contents
 
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
-  })
-})
+describe("adding new tasks", () => {
+  it("Visits the app root url", () => {
+    cy.visit("localhost:8080");
+
+    let tasks = [];
+    //get initial tasks
+    cy.get(".ui.segment")
+      .find(".card")
+      .each(($el, index, $list) => {
+        tasks.push($el[0].querySelector(".description").textContent);
+      });
+
+    //add new tasks
+    let newTasks = ["task A", "task B"];
+    newTasks.forEach((newTask) => {
+      tasks.unshift(newTask);
+      cy.get("input").type(newTask);
+      cy.get(".ui.action.input").within(() => {
+        cy.get("button").click();
+      });
+    });
+    cy.wait(500);
+
+    //check tasks
+    cy.get(".ui.segment")
+      .find(".card")
+      .each(($el, index, $list) => {
+        expect($el[0].querySelector(".description").textContent).to.equal(
+          tasks[index]
+        );
+      });
+  });
+});
