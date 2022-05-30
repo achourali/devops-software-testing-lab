a41d7813b4d3e32a4afda2c78263df83baff194c
achourali <achourali1999@gmail.com>
2022-05-30 21:05:56 +0100

    @frontend: e2e test:delete tasks test

--- frontend/tests/e2e/specs/test.js
+++ frontend/tests/e2e/specs/test.js
@@ -1,6 +1,40 @@
 // https://docs.cypress.io/api/table-of-contents
 
-describe("adding new tasks", () => {
+// describe("adding new tasks", () => {
+//   it("Visits the app root url", () => {
+//     cy.visit("localhost:8080");
+
+//     let tasks = [];
+//     //get initial tasks
+//     cy.get(".ui.segment")
+//       .find(".card")
+//       .each(($el, index, $list) => {
+//         tasks.push($el[0].querySelector(".description").textContent);
+//       });
+
+//     //add new tasks
+//     let newTasks = ["task A", "task B"];
+//     newTasks.forEach((newTask) => {
+//       tasks.unshift(newTask);
+//       cy.get("input").type(newTask);
+//       cy.get(".ui.action.input").within(() => {
+//         cy.get("button").click();
+//       });
+//     });
+//     cy.wait(500);
+
+//     //check tasks
+//     cy.get(".ui.segment")
+//       .find(".card")
+//       .each(($el, index, $list) => {
+//         expect($el[0].querySelector(".description").textContent).to.equal(
+//           tasks[index]
+//         );
+//       });
+//   });
+// });
+
+describe("deleting tasks", () => {
   it("Visits the app root url", () => {
     cy.visit("localhost:8080");
 
@@ -10,26 +44,31 @@ describe("adding new tasks", () => {
       .find(".card")
       .each(($el, index, $list) => {
         tasks.push($el[0].querySelector(".description").textContent);
-      });
+      })
+      .then(() => {
+        //delete last task
+        cy.get(".ui.segment").within(() => {
+          cy.get(".card")
+            .first()
+            .within(() => {
+              cy.get(".ui.red.icon.button").click();
+            });
+        });
 
-    //add new tasks
-    let newTasks = ["task A", "task B"];
-    newTasks.forEach((newTask) => {
-      tasks.unshift(newTask);
-      cy.get("input").type(newTask);
-      cy.get(".ui.action.input").within(() => {
-        cy.get("button").click();
-      });
-    });
-    cy.wait(500);
+        tasks.shift();
 
-    //check tasks
-    cy.get(".ui.segment")
-      .find(".card")
-      .each(($el, index, $list) => {
-        expect($el[0].querySelector(".description").textContent).to.equal(
-          tasks[index]
-        );
+        cy.wait(500).then(() => {
+          console.log(tasks);
+          //check tasks
+          cy.get(".ui.segment")
+            .find(".card")
+            .each(($el, index, $list) => {
+              console.log("test");
+              expect($el[0].querySelector(".description").textContent).to.equal(
+                tasks[index]
+              );
+            });
+        });
       });
   });
 });
