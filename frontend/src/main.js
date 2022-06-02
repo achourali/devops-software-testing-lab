30e542c3c8029c28609e4f6ce5f9cfeb8645169e
achourali <achourali1999@gmail.com>
2022-06-02 23:25:41 +0100

    changing backend url

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue'
 
 
 let app=createApp(App)
-app.config.globalProperties.backURL = 'http://localhost:3000'
+app.config.globalProperties.backURL = 'http://localhost:3000/api'
 app.config.globalProperties.socketioURL = 'ws://localhost:3001'
 app.mount('#app')
