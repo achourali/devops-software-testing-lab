a3a318b05ccb245dbd5d33b3380b5878100e71b2
achourali <achourali1999@gmail.com>
2022-06-04 23:10:04 +0100

    fix production urls

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
+let app = createApp(App);
+if (process.env.NODE_ENV == 'development') {
+  app.config.globalProperties.backURL = 'http://localhost:3000/api';
+  app.config.globalProperties.socketioURL = 'ws://localhost:3001';
+} else {
+  let hostname = window.location.hostname;
+  app.config.globalProperties.backURL = `http://${hostname}:3000/api`;
+  app.config.globalProperties.socketioURL = `ws://${hostname}:3001`;
+}
 
-let app=createApp(App)
-app.config.globalProperties.backURL = 'http://localhost:3000/api'
-app.config.globalProperties.socketioURL = 'ws://localhost:3001'
-app.mount('#app')
+app.mount('#app');
